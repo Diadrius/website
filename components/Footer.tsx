57b1140705d1fe5e4c61211992cb0d9c423e2016
Diadriu <59838858+Diadriu@users.noreply.github.com>
2025-10-29 17:54:41 +0100

    .

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,15 +7,16 @@ const Footer: React.FC = () => {
       <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-start text-text-light">
         {/* Left Section: Title and Copyright */}
         <div className="flex flex-col items-start text-left mb-6 md:mb-0">
-          <h3 className="font-serif text-lg font-semibold text-dark-green mb-2">Lotte Gasenbeek Psychologische Coaching</h3>
+          <h3 className="font-serif text-lg font-semibold text-dark-green mb-2">Lotte Gasenbeek psychologische begeleiding</h3>
           <p className="mt-2 text-xs text-gray-500">&copy; {new Date().getFullYear()} Lotte Gasenbeek. Alle rechten voorbehouden.</p>
         </div>
 
         {/* Center Section: Contact and Affiliation */}
         <div className="flex flex-col items-start text-left mb-6 md:mb-0">
           <p className="mb-1 text-sm text-gray-700">Praktijklocatie: Het Gebouw, Rotterdam</p>
-          <p className="mb-1 text-sm text-gray-700">Email: <a href="mailto:lottegasenbeek@gmail.com" className="text-gray-700 hover:text-ocher transition-colors">lottegasenbeek@gmail.com</a></p>
-          <p className="mt-2 text-sm">Aangesloten bij het <a href="https://www.psynip.nl/" target="_blank" rel="noopener noreferrer" className="underline hover:text-ocher transition-colors text-gray-700">Nederlands Instituut van Psychologen (NIP)</a></p>
+          <p className="mb-1 text-sm text-gray-700">Email: <a href="mailto:info@lottegasenbeek.nl" className="text-gray-700 hover:text-ocher transition-colors">info@lottegasenbeek.nl</a></p>
+          <p className="mt-2 text-sm">Aangesloten bij het <a href="https://nip.nl/" target="_blank" rel="noopener noreferrer" className="underline hover:text-ocher transition-colors text-gray-700">Nederlands Instituut van Psychologen (NIP)</a></p>
+          <img src="/website/media/images/nip.png" alt="NIP Logo" className="h-9 mt-2" />
         </div>
 
         {/* Right Section: Social Media */}
